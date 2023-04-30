7fcd623282c32536e6d031515da06df56c1f7840
chrystalio <chrystalio.k@outlook.com>
2023-05-01 06:54:06 +0700

    Setup main.jsx return each views based on route request

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import '@/assets/main.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from '@/views/Home.jsx'
+import Register from '@/views/auth/Register.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route index element={<Home/>}/>
+        <Route path="register" element={<Register />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
